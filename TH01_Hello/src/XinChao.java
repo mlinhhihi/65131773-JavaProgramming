b007cdfcc19591e252d0f1affdaef52053ecc872
mlinhhihi <ttml2k5@gmail.com>
2026-01-19 22:34:35 +0700

    TH01

---
+++ TH01_Hello/src/XinChao.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Xin chao. Toi la sinh vien khoa CNTT - NTU: Hoc lap trinh Java");
+		System.out.print("Trương Thị Mỹ Linh");
+	}
+
+}
