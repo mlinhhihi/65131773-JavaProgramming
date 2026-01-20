715140165fe4b4ea80015b188325328247a7f077
mlinhhihi <ttml2k5@gmail.com>
2026-01-20 14:31:41 +0700

    Update Xinchao.java

--- TH01/src/mlinh/Xinchao.java
+++ TH01/src/mlinh/Xinchao.java
@@ -15,6 +15,7 @@ public class Xinchao {
 		// TODO Auto-generated method stub
 		System.out.println("Xin chào. Tôi là sinh viên khóa 65 khoa CNTT - NTU");
 		System.out.println("Trương Thị Mỹ Linh: lập trình Java");
+		System.out.println("Học phần: Lập trình thiết bị di động 65.CNTT-2");
 		System.out.print("Thêm dòng mới");	}
 
 }
