96b34d6635a4f8adf78aed02678837f629c232ee
mlinhhihi <ttml2k5@gmail.com>
2026-01-20 14:25:29 +0700

    Create Xinchao.java

---
+++ TH01/src/mlinh/Xinchao.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package mlinh;
+
+/**
+ * 
+ */
+public class Xinchao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Xin chào. Tôi là sinh viên khóa 65 khoa CNTT - NTU");
+		System.out.print("Trương Thị Mỹ Linh: lập trình Java");
+
+	}
+
+}
